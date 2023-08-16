8be2348837d6ac2af012e8a6a6ee70543d46f11b
Aditya Saran <135217588+adhi-26@users.noreply.github.com>
2023-08-16 16:21:22 +0530

    Get a wordcloud instance

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from utils.download_reviews import download_reviews
 from utils.preprocessing_text import preprocess_text
 import MoviePage
 import ReviewsPage
-
+from wordcloud import WordCloud
 def load_pickles():
     global model
     with open('pickles/SVC3304548969/SVC3304548969_model.sav', 'rb') as f:
@@ -19,7 +19,9 @@ def get_reviews(id):
     reviews = pd.DataFrame(download_reviews(id))
     processed_reviews = reviews.Review.map(preprocess_text)
     transformed = vectorizer.transform(processed_reviews)
-    return reviews, model.predict(transformed)
+    reviews_combined = ' '.join(processed_reviews)
+    wordcloud = WordCloud(width=1000, height=800, margin=0).generate(reviews_combined)
+    return reviews, model.predict(transformed), wordcloud
 
 movies = pd.read_csv('MovieData/metadata.csv', index_col='imdb-id')
 load_pickles()
@@ -36,8 +38,8 @@ selected_movieid = st.selectbox(
 
 if st.button('Show Results'):
     with st.spinner('Downloading Reviews. This might take a while...'):
-        reviews, predictions = get_reviews(selected_movieid)
-    MoviePage.RenderPage(movies.loc[selected_movieid], predictions)
+        reviews, predictions, wordcloud = get_reviews(selected_movieid)
+    MoviePage.RenderPage(movies.loc[selected_movieid], predictions, wordcloud)
     ReviewsPage.RenderPage(reviews)
     
 
